5f7123037aa487facf7e2c1ab4d4ca5913484a84
Muhammad Ali <muhammadalikhurram01@gmail.com>
2025-08-02 16:27:03 +0500

    Video #4 - Let, Const, and Var
    learned about let, const, and var.

---
+++ 01-basics/01_var.js
@@ -0,0 +1,22 @@
+const account_id = 144553
+let account_email = "example@gmail.com" //recommended method
+var account_password = "12345" //not recommended
+account_city = "jaipur" //not recommended
+let account_state;
+
+// account_id = 2
+// console.log(account_id);
+
+account_email = "hc@hc.com"
+account_password = "212121"
+account_city = "bangluru"
+
+console.table([account_id, account_email, account_password, account_city, account_state])
+
+account_state = "UP"
+
+console.table([account_id, account_email, account_password, account_city, account_state])
+
+account_state = 1
+
+console.table([account_id, account_email, account_password, account_city, account_state])
