1860c0ed73a816a887c3eeca5d1f169916751508
Muhammad Ali <121323318+01-muhammadalikhurram@users.noreply.github.com>
2025-08-03 05:42:23 +0000

    Video #5 - Data Types
    Learned about Data Types and types of values they can hold such as number, big int, string, bool, null, undefined, symbol, object
    and also "typeof" function

---
+++ 01-basics/02_data_types.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/*
+
+we are using nodejs not browser
+alert("hello")
+alert(3+3)
+
+*/
+
+// code readability matters
+console.log(3 + 3) 
+console.log("Muhammad Ali")
+
+let name = "hitesh" //string
+let age = 21 //number/integer
+let is_logged_in = false //boolean
+let state;
+
+// Primitive Data Types:
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// Symbol => unique
+
+// Object
+
+console.log(typeof age);
