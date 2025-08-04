9c6a589cff8fbe4af3cb0a1089d93528d9054e37
Muhammad Ali <muhammadalikhurram01@gmail.com>
2025-08-04 11:12:33 +0500

    Video #7 - Conversions and Operations
    Learned about conversions through operations such
    as:
    string + number => string+number => string
    number + number + string => number+string => string
    did operations on boolean also

--- 01-basics/03_conversion_operations.js
+++ 01-basics/03_conversion_operations.js
@@ -1,3 +1,5 @@
+/*
+Video #6 - Datatype conversions
 let score = undefined
 
 // const {score} = req.body //something unrelated for now => don't know what it is
@@ -22,3 +24,39 @@ console.log(boolean_is_logged_in);
 console.log(is_logged_in);
 
 //other conversions also possible
+*/
+
+//Video #7 - String to number conersion
+
+let value = 3
+let neg_val = -value
+// console.log(neg_val)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Muhammad Ali"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(typeof (1 + 2 + "2"));
+
+// console.log(true+)
+// console.log(+true)
+// console.log(true+true)
+// console.log(+"")
+
+// usage of prefix and postfix operator
+let game_counter = 100
+game_counter++
+++game_counter
